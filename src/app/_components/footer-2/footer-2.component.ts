31ee9db1a6f52df086f943e0a681e724c25591ca
Lucas <lucasgoncalves08@live.com>
2024-02-22 09:19:41 -0300

    creating footers

---
+++ src/app/_components/footer-2/footer-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-2.component.html',
+  styleUrl: './footer-2.component.scss'
+})
+export class Footer2Component {
+
+}
