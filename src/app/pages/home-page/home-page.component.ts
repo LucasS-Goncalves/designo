0c991608f1af64063f1966996ad42b1a4c36a06a
Lucas <lucasgoncalves08@live.com>
2024-02-22 10:28:07 -0300

    starting to work on footer 2

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../_components/navbar/navbar.component';
+import { Footer2Component } from '../../_components/footer-2/footer-2.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, Footer2Component],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
