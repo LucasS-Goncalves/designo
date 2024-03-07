1a7045ffcfa15a3065a7507722090f021a63a24d
Lucas <lucasgoncalves08@live.com>
2024-03-07 13:12:29 -0300

    creating web design component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { WebDesignComponent } from './pages/web-design/web-design.component';
 
 export const routes: Routes = [
   {
     path: '', component: HomePageComponent
+  },
+  {
+    path: 'web-design', component: WebDesignComponent
   }
 ];
